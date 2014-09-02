0e28a2a69a550a7fb628968627df0fb4bba80230
Robert Hedges <hedges@ieee.org>
2014-09-02 08:07:33 -0500

    Initial check-in of Vortexbox Packer files.

---
+++ scripts/vmtools.sh
@@ -0,0 +1,11 @@
+if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
+yum -y install bzip2 net-tools
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+fi
+
+
