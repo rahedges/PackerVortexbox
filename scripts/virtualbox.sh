67a726d8bf8b9892d125b40c9ceff59b724e9d5a
Robert Hedges <rhedges@truefitness.com>
2014-09-03 10:22:58 -0500

    Many updates, to get ssh working correctly and Vbox guest addtions.

---
+++ scripts/virtualbox.sh
@@ -0,0 +1,21 @@
+
+# Required for the VirtualBox Guest Additions for Linux
+# http://www.virtualbox.org/manual/ch04.html
+yum install --assumeyes bzip2 dkms gcc kernel-devel make
+  
+# Required to prevent VBoxLinuxAdditions.run from throwing (harmless) errors
+yum install --assumeyes which
+  
+# Install the VirtualBox Guest Additions
+mkdir /mnt/cdrom
+mount /root/VBoxGuestAdditions.iso /mnt/cdrom
+sh /mnt/cdrom/VBoxLinuxAdditions.run
+  
+# Cleanup
+umount /mnt/cdrom
+rmdir /mnt/cdrom
+rm --force /root/VBoxGuestAdditions.iso
+    
+# Fix permissions and SELinux context for Packer’s virtualbox_version_file
+chmod 0644 /etc/virtualbox-version
+chcon system_u:object_r:etc_t:s0 /etc/virtualbox-version
